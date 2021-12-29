13b3516ff571b697812bce4e4f99372636121218
Alexander Czigler <dev@ilix.se>
2021-12-29 20:57:52 +0100

    feat: work in progress

---
+++ src/components/Link.svelte
@@ -0,0 +1,8 @@
+<script>
+  export let icon;
+  export let name;
+  export let url;
+</script>
+
+<i class="iconoir-{icon}" />
+<a href={url}>{name}</a>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,16 +1,32 @@
 <script>
   import '../app.scss';
+
+  import Link from '../components/Link.svelte';
+
+  import links from '../lib/links.json';
 </script>
 
 <h1>Alexander Czigler</h1>
 <h2>Backend Engineer at <a href="https://iteam.se">Iteam</a></h2>
 
-<ul>
-  <li>
-    <i class="iconoir-github" />
-    <a href="https://github.com/alexanderczigler">GitHub</a>
-  </li>
-</ul>
+<h3>Hello!</h3>
+<p>
+  My name is Alexander and I live in Stockholm, Sweden. I work as an IT consultant mainly focusing
+  on system development and agile coaching/mentoring.
+</p>
+
+{#each links as category}
+  <div>
+    <h3>{category.name}</h3>
+    <ul>
+      {#each category.links as link}
+        <li>
+          <Link icon={link.icon} name={link.name} url={link.url} />
+        </li>
+      {/each}
+    </ul>
+  </div>
+{/each}
 
 <style>
 </style>
