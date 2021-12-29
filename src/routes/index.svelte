28c66ecc0a429e2fde67bc9aa81799f7774352c8
Alexander Czigler <git@ilix.se>
2021-12-29 12:22:22 +0100

    chore: first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,41 @@
+<script>
+  import '../app.scss';
+</script>
+
+<h1>
+  <i class="iconoir-star-outline" /> Stars
+</h1>
+<h2>Welcome Alexander</h2>
+
+<div>
+  <p>Finance</p>
+  <ul>
+    <li>Avanza</li>
+    <li>BetterWealth</li>
+    <li>Coinbase</li>
+    <li>Lysa</li>
+    <li>Kameo</li>
+    <li>SEB</li>
+  </ul>
+</div>
+
+<div>
+  <p>Inspiration</p>
+  <ul>
+    <li>Patreon</li>
+  </ul>
+</div>
+
+<div>
+  <p>Work</p>
+  <ul>
+    <li>BitBucket</li>
+    <li>Docker Hub</li>
+    <li>GitHub</li>
+    <li>GitLab</li>
+    <li>Visualstudio</li>
+  </ul>
+</div>
+
+<style>
+</style>
