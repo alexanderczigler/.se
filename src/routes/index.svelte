fcb49721f47aded89315f06ccd3cbdc0d0d26bb0
Alexander Czigler <dev@ilix.se>
2021-12-31 18:12:39 +0100

    refactor: content styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,62 +6,84 @@
 </script>
 
 <Header />
-<p>
-  I am a self-taught software developer currently working as a backend engingeer at <a
-    href="https://iteam.se/">Iteam Solutions</a
-  > in Stockholm.
-</p>
 
-<p>
-  My passion for software and the web began in the late 90's when I started learning basic HTML and
-  making homepages. When I got DSL in the early 00's I built my first server and started learning
-  about linux and setting up networks, web servers and firewalls. Since then I have been learning
-  more and more all the time by continuous exploration and by surrounding myself with talented
-  people who inspire me to learn and grow.
-</p>
+<div id="content">
+  <h3>Hello</h3>
+  <p>
+    I am a self-taught software developer currently working as a backend engingeer at <a
+      href="https://iteam.se/">Iteam Solutions</a
+    > in Stockholm.
+  </p>
 
-<p>
-  Outside of computers and technology I enjoy reading, working out and being outdoors in nature. One
-  of my latest hobbies that I have picked up is dancing. I took classes learning Lindy Hop a few
-  years ago and after that I switched to poledancing which I still practice regularly. Along with
-  dancing I also pratice yoga and some basic acrobatics such as handbalancing.
-</p>
+  <p>
+    My passion for software and the web began in the late 90's when I started learning basic HTML
+    and making homepages. When I got DSL in the early 00's I built my first server and started
+    learning about linux and setting up networks, web servers and firewalls. Since then I have been
+    learning more and more all the time by continuous exploration and by surrounding myself with
+    talented people who inspire me to learn and grow.
+  </p>
 
-<h3>Experience</h3>
+  <p>
+    Outside of computers and technology I enjoy reading, working out and being outdoors in nature.
+    One of my latest hobbies that I have picked up is dancing. I took classes learning Lindy Hop a
+    few years ago and after that I switched to poledancing which I still practice regularly. Along
+    with dancing I also pratice yoga and some basic acrobatics such as handbalancing.
+  </p>
 
-<p>
-  I am going to add my resumé here soon. In the mean time, check out my <a
-    href="https://linkedin.com/in/alexanderczigler">LinkedIn profile</a
-  >.
-</p>
+  <h3>Experience</h3>
 
-<h3>Social</h3>
+  <p>
+    I am going to add my resumé here soon. In the mean time, check out my <a
+      href="https://linkedin.com/in/alexanderczigler">LinkedIn profile</a
+    >.
+  </p>
 
-<p>
-  If you are curious about some of the non work-related aspects of me, you can find me on a few
-  social platforms as well. Feel free to send me a friend request if you happen to use any of them.
-</p>
+  <h3>Social</h3>
 
-<p>
-  I use <a href="https://goodreads.com/alexanderczigler">Goodreads</a> to track the books I read. I
-  rarely actually write reviews but I am quite active keeping my own library and to-read list
-  updated. I also enjoy learning new languages on
-  <a href="https://www.duolingo.com/profile/alexanderczigler">Duolingo</a> where I am currently learning
-  Italian.
-</p>
+  <p>
+    If you are curious about some of the non work-related aspects of me, you can find me on a few
+    social platforms as well. Feel free to send me a friend request if you happen to use any of
+    them.
+  </p>
 
-<p>
-  If you are into deep house, techno or retrowave, check out my <a
-    href="https://soundcloud.com/alexanderczigler">SoundCloud</a
-  >
-  profile. And last but not least you can find me on
-  <a href="https://www.strava.com/athletes/60476104">Strava</a> where I track most of my workouts and
-  walks.
-</p>
+  <p>
+    I use <a href="https://goodreads.com/alexanderczigler">Goodreads</a> to track the books I read.
+    I rarely actually write reviews but I am quite active keeping my own library and to-read list
+    updated. I also enjoy learning new languages on
+    <a href="https://www.duolingo.com/profile/alexanderczigler">Duolingo</a> where I am currently learning
+    Italian.
+  </p>
+
+  <p>
+    If you are into deep house, techno or retrowave, check out my <a
+      href="https://soundcloud.com/alexanderczigler">SoundCloud</a
+    >
+    profile. And last but not least you can find me on
+    <a href="https://www.strava.com/athletes/60476104">Strava</a> where I track most of my workouts and
+    walks.
+  </p>
+</div>
 
 <Footer />
 
 <style>
+  #content {
+    margin: 2em 0;
+    padding-bottom: 1em;
+  }
+
+  @media (min-width: 768px) {
+    #content {
+    }
+  }
+
+  @media (min-width: 950px) {
+    #content {
+      margin: 2em auto;
+      max-width: 950px;
+    }
+  }
+
   a {
     background-color: var(--accent);
     color: var(--text-fade);
@@ -76,6 +98,6 @@
 
   p {
     font-weight: 300;
-    margin: 1em 1em;
+    margin: 1em 24px;
   }
 </style>
