5f614367b730c9694001600e79c06d2008ac2b91
Alexander Czigler <dev@ilix.se>
2022-01-05 11:36:43 +0100

    fix: show all experience

--- src/routes/resume.json.js
+++ src/routes/resume.json.js
@@ -1,12 +1,10 @@
 import experiences from '$lib/resume';
 
 export async function get() {
-  const body = Object.keys(experiences)
-    .slice(0, 4)
-    .map((slug) => ({
-      slug,
-      ...experiences[slug],
-    }));
+  const body = Object.keys(experiences).map((slug) => ({
+    slug,
+    ...experiences[slug],
+  }));
 
   return {
     status: 200,
